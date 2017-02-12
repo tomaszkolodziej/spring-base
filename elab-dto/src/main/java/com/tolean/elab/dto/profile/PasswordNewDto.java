2ab42ed4b1151ab1ddeda33b1e7c92eeb39bc214
Tomasz Kolodziej <tomasz.kolodziej@poczta.pl>
2017-02-12 18:17:33 +0100

    encode password

--- elab-dto/src/main/java/com/tolean/elab/dto/profile/PasswordNewDto.java
+++ elab-dto/src/main/java/com/tolean/elab/dto/profile/PasswordNewDto.java
@@ -1,11 +1,17 @@
 package com.tolean.elab.dto.profile;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Created by Tomasz Kołodziej
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PasswordNewDto {
 
     private String oldPassword;
