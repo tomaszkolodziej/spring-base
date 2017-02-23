95688775f57638fa2fc5b25ecc296c98d675d58a
Tomasz Kołodziej <tomasz.kolodziej@coi.gov.pl>
2017-02-23 14:07:03 +0100

    settings.js

--- elab-dto/src/main/java/com/tolean/elab/dto/profile/ProfileLightViewDto.java
+++ elab-dto/src/main/java/com/tolean/elab/dto/profile/ProfileLightViewDto.java
@@ -1,25 +1,13 @@
 package com.tolean.elab.dto.profile;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
  * Created by tomasz.kolodziej@poczta.pl
  */
-@Data
 @Builder
 @NoArgsConstructor
-@AllArgsConstructor
-public class ProfileLightViewDto {
-
-    private Long id;
-    private String login;
-    private String name;
-    private String firstName;
-    private String lastName;
-    private String email;
-    private boolean active;
+public class ProfileLightViewDto extends AbstractProfileViewDto {
 
 }
