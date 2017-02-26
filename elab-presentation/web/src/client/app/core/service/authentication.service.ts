1f86572bb857faefd90df6449b14fb77bcf35dda
Tomasz Kolodziej <tomasz.kolodziej@poczta.pl>
2017-02-26 19:21:46 +0100

    wyswietlanie informacji o wersji aplikacji

--- elab-presentation/web/src/client/app/core/service/authentication.service.ts
+++ elab-presentation/web/src/client/app/core/service/authentication.service.ts
@@ -5,8 +5,6 @@ import {Http, Headers, Response} from "@angular/http";
 import {SystemService} from "./system.service";
 import {MsgService} from "./msg.service";
 
-import {Error} from '../model/error';
-
 @Injectable()
 export class AuthenticationService {
 
@@ -33,7 +31,6 @@ export class AuthenticationService {
                     this.token = authenticationResponseData.token;
 
                     this.addProfileToLocalStorage(authenticationResponseData);
-                    this.addSystemInfoToLocalStorage();
 
                     return true;
                 } else {
@@ -46,22 +43,6 @@ export class AuthenticationService {
         let currentProfile = authenticationResponse.profile;
         currentProfile.token = authenticationResponse.token;
         localStorage.setItem("currentProfile", JSON.stringify(currentProfile));
-    }
-
-    private addSystemInfoToLocalStorage() {
-        let systemInfo = {};
-
-        this.systemService.getSystemInfo().subscribe(
-            (systemInfoData: any) => {
-                systemInfo = systemInfoData;
-            },
-            (err: any) => {
-                this.msgService.error(new Error(err));
-            },
-            () => {
-                localStorage.setItem("systemInfo", JSON.stringify(systemInfo));
-            }
-        );
     }
 
     logout(): void {
